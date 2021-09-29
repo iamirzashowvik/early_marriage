600bfa21f60870ebe64c655d3262fb2da6502d11
iamirzashowvik <showvikmirza@gmail.com>
2021-09-29 10:27:31 +0600

    quiz up

--- lib/app/quiz/quiz.dart
+++ lib/app/quiz/quiz.dart
@@ -41,25 +41,34 @@ class _QuizHomeState extends State<QuizHome> {
     super.initState();
   }
 
+  List<dynamic> cans = [];
+  List<dynamic> wans = [];
   void selectedOption(String option, String cA, int index, int no) async {
     if (option == cA) {
-      Get.snackbar(
-        'Correct',
-        'Your answer is write, $no',
-        colorText: Colors.green,
-        backgroundColor: Colors.white,
-      );
+      cans.contains({"no": no, "ca": cA})
+          ? cans.removeWhere((element) => element == {"no": no, "ca": cA})
+          : cans.add({"no": no, "ca": cA});
+      //  cans.add({"no": no, "ca": cA});
+      // Get.snackbar(
+      //   'Correct',
+      //   'Your answer is write, $no',
+      //   colorText: Colors.green,
+      //   backgroundColor: Colors.white,
+      // );
       // Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.")
       //     .show();
     } else {
       // Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.")
       //     .show();
-      Get.snackbar(
-        'Wrong',
-        'Your answer is wrong, $no',
-        colorText: Colors.red,
-        backgroundColor: Colors.white,
-      );
+      wans.contains({"no": no, "ca": cA})
+          ? wans.removeWhere((element) => element == {"no": no, "ca": cA})
+          : wans.add({"no": no, "ca": cA});
+      // Get.snackbar(
+      //   'Wrong',
+      //   'Your answer is wrong, $no',
+      //   colorText: Colors.red,
+      //   backgroundColor: Colors.white,
+      // );
     }
   }
 
@@ -73,128 +82,145 @@ class _QuizHomeState extends State<QuizHome> {
       ),
       body: itemX == false
           ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: lengthx,
-              itemBuilder: (context, i) {
-                // final itemXx = quizmodelFromJson(jsonEncode(itemy.data()));
+          : Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: ListView.builder(
+                    itemCount: lengthx,
+                    itemBuilder: (context, i) {
+                      // final itemXx = quizmodelFromJson(jsonEncode(itemy.data()));
 
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      //height: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 3,
-                            offset: Offset(0, 1), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Padding(
+                      return Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Center(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Text(
-                                '${itemXx[i].no}. ${itemXx[i].q}',
-                                style: TextStyle(fontSize: 25),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 100,
+                            //height: 200,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 2,
+                                  blurRadius: 3,
+                                  offset: Offset(
+                                      0, 1), // changes position of shadow
+                                ),
+                              ],
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    '${itemXx[i].no}. ${itemXx[i].q}',
+                                    style: TextStyle(fontSize: 25),
+                                  ),
+                                  GestureDetector(
+                                      onTap: () {
+                                        selectedOption(itemXx[i].a,
+                                            itemXx[i].ca, i, itemXx[i].no);
+                                      },
+                                      child: Container(
+                                          height: 35,
+                                          width: 250,
+                                          child: Row(
+                                            children: [
+                                              Icon(Icons.circle_outlined),
+                                              SizedBox(
+                                                width: 5,
+                                              ),
+                                              Text(
+                                                itemXx[i].a,
+                                                style: TextStyle(fontSize: 20),
+                                              ),
+                                            ],
+                                          ))),
+                                  GestureDetector(
+                                      onTap: () {
+                                        selectedOption(itemXx[i].b,
+                                            itemXx[i].ca, i, itemXx[i].no);
+                                      },
+                                      child: Container(
+                                          height: 35,
+                                          width: 250,
+                                          child: Row(
+                                            children: [
+                                              Icon(Icons.circle_outlined),
+                                              SizedBox(
+                                                width: 5,
+                                              ),
+                                              Text(
+                                                itemXx[i].b,
+                                                style: TextStyle(fontSize: 20),
+                                              ),
+                                            ],
+                                          ))),
+                                  GestureDetector(
+                                      onTap: () {
+                                        selectedOption(itemXx[i].c,
+                                            itemXx[i].ca, i, itemXx[i].no);
+                                      },
+                                      child: Container(
+                                          height: 35,
+                                          width: 250,
+                                          child: Row(
+                                            children: [
+                                              Icon(Icons.circle_outlined),
+                                              SizedBox(
+                                                width: 5,
+                                              ),
+                                              Text(
+                                                itemXx[i].c,
+                                                style: TextStyle(fontSize: 20),
+                                              ),
+                                            ],
+                                          ))),
+                                  GestureDetector(
+                                      onTap: () {
+                                        selectedOption(itemXx[i].d,
+                                            itemXx[i].ca, i, itemXx[i].no);
+                                      },
+                                      child: Container(
+                                          height: 35,
+                                          width: 250,
+                                          child: Row(
+                                            children: [
+                                              Icon(Icons.circle_outlined),
+                                              SizedBox(
+                                                width: 5,
+                                              ),
+                                              Text(
+                                                itemXx[i].d,
+                                                style: TextStyle(fontSize: 20),
+                                              ),
+                                            ],
+                                          ))),
+                                ],
                               ),
-                              GestureDetector(
-                                  onTap: () {
-                                    selectedOption(itemXx[i].a, itemXx[i].ca, i,
-                                        itemXx[i].no);
-                                  },
-                                  child: Container(
-                                      height: 35,
-                                      width: 250,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.circle_outlined),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text(
-                                            itemXx[i].a,
-                                            style: TextStyle(fontSize: 20),
-                                          ),
-                                        ],
-                                      ))),
-                              GestureDetector(
-                                  onTap: () {
-                                    selectedOption(itemXx[i].b, itemXx[i].ca, i,
-                                        itemXx[i].no);
-                                  },
-                                  child: Container(
-                                      height: 35,
-                                      width: 250,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.circle_outlined),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text(
-                                            itemXx[i].b,
-                                            style: TextStyle(fontSize: 20),
-                                          ),
-                                        ],
-                                      ))),
-                              GestureDetector(
-                                  onTap: () {
-                                    selectedOption(itemXx[i].c, itemXx[i].ca, i,
-                                        itemXx[i].no);
-                                  },
-                                  child: Container(
-                                      height: 35,
-                                      width: 250,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.circle_outlined),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text(
-                                            itemXx[i].c,
-                                            style: TextStyle(fontSize: 20),
-                                          ),
-                                        ],
-                                      ))),
-                              GestureDetector(
-                                  onTap: () {
-                                    selectedOption(itemXx[i].d, itemXx[i].ca, i,
-                                        itemXx[i].no);
-                                  },
-                                  child: Container(
-                                      height: 35,
-                                      width: 250,
-                                      child: Row(
-                                        children: [
-                                          Icon(Icons.circle_outlined),
-                                          SizedBox(
-                                            width: 5,
-                                          ),
-                                          Text(
-                                            itemXx[i].d,
-                                            style: TextStyle(fontSize: 20),
-                                          ),
-                                        ],
-                                      ))),
-                            ],
+                            ),
                           ),
                         ),
-                      ),
-                    ),
+                      );
+                    },
                   ),
-                );
-              },
+                ),
+                TextButton(
+                    onPressed: () {
+                      Get.defaultDialog(
+                          title: "Right Answer:[${cans}]",
+                          middleText: 'Wrong Anser:[${wans}]');
+                      cans = [];
+                      wans = [];
+                    },
+                    child: Text('Check Result'))
+              ],
             ),
     );
   }
